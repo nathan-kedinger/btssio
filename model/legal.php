3beb8cb46c4e82ef026470d146f0f2689ce045b0
nathan-kedinger <51140084+nathan-kedinger@users.noreply.github.com>
2023-04-18 13:52:49 +0200

    update legal

--- model/legal.php
+++ model/legal.php
@@ -6,44 +6,44 @@ require_once '../view/content/head.php';
 
 <body>
     <main>
-    <h1>MENTIONS LÉGALES</h1>
-<p>
-    Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en l'économie numérique, il est précisé aux utilisateurs du site bts-sio-kedinger.fr l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi.
-</p>
-<h2>
-    Edition du site 
-</h2>
-<p>
-    Le présent site, accessible à l’URL www.bts-sio-kedinger.fr (le « Site »), est édité par :
-</p>
-<p>
-    Nathan Kedinger, résidant 1 Petite Avenue Saint-Clair 44100 Nantes, de nationalité Française (France), né(e) le 07/01/1993, 
-</p>
-
-<h2>
-Hébergement
-</h2>
-<p>
-Le Site est hébergé par la société Hostinger International LTD, situé , (contact téléphonique ou email : https://www.hostinger.fr/contact).
-</p>
-<h2>
-Directeur de publication 
-</h2>
-<p>
-Le Directeur de la publication du Site est Nathan Kedinger.
-</p>
-<h2>
-Nous contacter 
-</h2>
-<p>
-
-    Par téléphone : +33671392430
-    <br>Par email : nathan.kedinger@gmail.com
-    <br>Par courrier : 1 Petite Avenue Saint-Clair 44100 Nantes
-    
-    <br> <br>Génération des mentions légales par Legalstart.
-</p>
-
+        <article>
+            <h1>MENTIONS LÉGALES</h1>
+            <p>
+                Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en l'économie numérique, il est précisé aux utilisateurs du site bts-sio-kedinger.fr l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi.
+            </p>
+            <h2>
+                Edition du site 
+            </h2>
+            <p>
+                Le présent site, accessible à l’URL www.bts-sio-kedinger.fr (le « Site »), est édité par :
+            </p>
+            <p>
+                Nathan Kedinger, résidant 1 Petite Avenue Saint-Clair 44100 Nantes, de nationalité Française (France), né(e) le 07/01/1993, 
+            </p>
+            
+            <h2>
+                Hébergement
+            </h2>
+            <p>
+                Le Site est hébergé par la société Hostinger International LTD, situé , (contact téléphonique ou email : https://www.hostinger.fr/contact).
+            </p>
+            <h2>
+                Directeur de publication 
+            </h2>
+            <p>
+                Le Directeur de la publication du Site est Nathan Kedinger.
+            </p>
+            <h2>
+                Nous contacter 
+            </h2>
+            <p>
+                Par téléphone : +33671392430
+                <br>Par email : nathan.kedinger@gmail.com
+                <br>Par courrier : 1 Petite Avenue Saint-Clair 44100 Nantes
+                
+                <br> <br>Génération des mentions légales par Legalstart.
+            </p>
+        </article>
     </main>
 
     <aside>
